ad2dca286b2ae1021aa93729006c7ffdf6f6bab1
hafizur rahman <hafizurrahmanbu@gmail.com>
2023-10-28 01:30:07 +0600

    nav designed done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css'
+import logo from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="logo_section">
+                <img src={logo} alt="" />
+            </div>
+            <div className="nav_content_section">
+                <a href="/order">Order</a>
+                <a href="/order-review">Order review</a>
+                <a href="/inventory">Manage Inventory</a>
+                <a href="/login">LogIn</a>
+            </div>
+
+        </div>
+    );
+};
+
+export default Header;
