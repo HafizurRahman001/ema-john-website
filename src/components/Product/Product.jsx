55c6a851471eee60fd7ca127b3485a3b1685de18
hafizur rahman <hafizurrahmanbu@gmail.com>
2023-10-28 02:59:11 +0600

    product display section is done

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const { img, seller, name, quantity, price, ratings } = props.product;
+    return (
+        <div className='product_card'>
+            <div className="product_img">
+                <img src={img} alt="" />
+            </div>
+            <div className="product_body">
+                <h4>{name.slice(0, 25)}</h4>
+                <p>Price:${price}</p>
+            </div>
+            <div className="card_bottom_up_container">
+                <p>Manufacturer: {seller}</p>
+                <p>Ratings: {ratings}</p>
+            </div>
+            <div className="">
+                <button className='product_footer'>Add to Cart <span style={{ fontSize: '17px' }} className="material-symbols-outlined">
+                    add_shopping_cart
+                </span></button>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css'
+import Product from '../Product/Product';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+            .then(response => response.json())
+            .then(data => setProducts(data))
+    }, []);
+
+    // console.log(products)
+    return (
+        <div className='shop_container'>
+            <div className="products_container">
+                {
+                    products.map(product => <Product
+                        key={product?.id}
+                        product={product}
+                    />)
+                }
+            </div>
+            <div className="cart_container">
+                <h3>Order summary</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
